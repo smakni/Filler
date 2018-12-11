57a6d1283f5d8c53ce427d10ab4faf7c664995fb
Sabri MAKNI <sabrimakni@gmail.com>
2018-12-11 18:29:01 +0100

    clean print

--- src/save_data.c
+++ src/save_data.c
@@ -6,7 +6,7 @@
 /*   By: smakni <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/10 17:59:44 by smakni            #+#    #+#             */
-/*   Updated: 2018/12/10 23:18:36 by smakni           ###   ########.fr       */
+/*   Updated: 2018/12/11 18:25:38 by smakni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,7 @@ static	void	save_map_data(char *line, t_player *p)
 	p->m_y = ft_atoi(&line[i++]);
 	i += find_space(line, i);
 	p->m_x = ft_atoi(&line[i]);
-	p->map = ft_memalloc(sizeof(char *) * p->m_y);
-	p->check = 1;
+	p->map = ft_memalloc(sizeof(char *) * (p->m_y + 1));
 }
 
 static	void	save_piece_data(char *line, t_player *p)
@@ -47,55 +46,53 @@ static	void	save_piece_data(char *line, t_player *p)
 	p->piece = ft_memalloc(sizeof(char *) * p->p_y);
 }
 
-static	void	first_read(int fd, char *line, t_player *p)
+static	void	first_read(char *line, t_player *p)
 {
 	if (p->nb == 0)
-	{	
 		p->nb = ft_atoi(&(line[10]));
-		ft_dprintf(fd, "p = %d\n", p->nb);
-	}
-	else if (line[1] == 'l' && p->check == 0)
-	{
+	else if (line[1] == 'l' && p->check_read == 0)
 		save_map_data(line, p);
-		ft_dprintf(fd, "map = %d\n", p->m_y);
-	}
 	else
 	{	
+		p->check_read = 1;
 		save_piece_data(line, p);
-		ft_dprintf(fd, "piece = %d\n", p->p_y);
 	}
 }
 
-void				save_data(int fd, t_player *p)
+int				save_data(t_player *p) //seg protection
 {
-	int i;
+	int i; 
 	int j;
+	int check;
 	char *line;
-	int 		check;
 
 	check = 0;
 	i = 0;
 	j = 0;
-	while (get_next_line(0, &line) > 0)
+	if (get_next_line(0, &line) > 0)
 	{
-		/*(if (line[0] == 'P' || line[0] == '$')
-		{	
-			first_read(fd, line, p);
-			check = 1;
-		}
+		if (line == NULL)
+			exit(0);
+		if (line[0] == 'P' || line[0] == '$')
+			first_read(line, p);
 		else if (line[0] == ' ' || ft_isdigit(line[0]) == 1)
 		{
-			p->map[i] = ft_strdup(line);	
-			ft_dprintf(fd, "p->map[%2d] = %s\n", i, p->map[i]);
-			i++;
+			if (p->check_read == 0)
+				p->map[p->i] = ft_strdup(line);
+			else
+				ft_strcpy(p->map[p->i], line);	
+			p->i++;
+			check = 1;
 		}
 		else if (line[0] == '.' || line[0] == '*')
 		{
-			p->piece[j] = ft_strdup(line);	
-			ft_dprintf(fd, "p->piece[%2d] = %s\n", j, p->piece[j]);
-			j++;
-		}*/
-		ft_dprintf(fd, "debug\n");
+			p->piece[p->j] = ft_strdup(line);	
+			p->j++;
+			check = 2;
+		}
 		ft_strdel(&line);
+		if (check == 2 && p->j == p->p_y && p->i == p->m_y + 1)	
+			return (1);
 	}
+	return (0);
 }
