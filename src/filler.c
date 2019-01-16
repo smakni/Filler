a00964055e77ff7c62c9d1169bae9824376db221
Sabri MAKNI <sabrimakni@gmail.com>
2019-01-16 16:36:22 +0100

    add analyse_map

--- src/filler.c
+++ src/filler.c
@@ -6,7 +6,7 @@
 /*   By: smakni <smakni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/04 19:06:40 by smakni            #+#    #+#             */
-/*   Updated: 2018/12/14 18:37:15 by smakni           ###   ########.fr       */
+/*   Updated: 2019/01/16 13:31:15 by smakni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,8 @@ int		main(void)
 	{
 		if (save_data(p) == 1)
 		{
-//			print_data(fd, p);
+			analyse_map(fd, p);
+			print_data(fd, p);
 			algo(fd, p);
 			ft_tabdel(p->piece, p->p_y);
 			if (p->r_y == 0 && p->r_x == 0)
