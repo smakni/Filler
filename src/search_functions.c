28ed4803a4721c60d53c9dcf345ea1cfdafe2db6
Sabri MAKNI <sabrimakni@gmail.com>
2019-01-16 16:35:55 +0100

    add heatmap

--- src/search_functions.c
+++ src/search_functions.c
@@ -6,37 +6,71 @@
 /*   By: smakni <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/13 14:27:01 by smakni            #+#    #+#             */
-/*   Updated: 2018/12/13 17:11:48 by smakni           ###   ########.fr       */
+/*   Updated: 2019/01/16 13:31:08 by smakni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <filler.h>
 
-void	find_opponent(t_player *p)
+static	void	fill_map(t_player *p, int y, int x, char c)
 {
-	int x;
+	int i;
+
+	i = 0;
+	while (i < 2)
+	{
+		if (x + i <= p->m_x && p->map[y][x + i] == '.')
+			p->map[y][x + i] = c;
+		if (x - i >= 0 && p->map[y][x - i] == '.')
+			p->map[y][x - i] = c;
+		if (y + i < p->m_y && p->map[y + i][x] == '.')
+			p->map[y + i][x] = c;
+		if (y - i >= 0 && p->map[y - i][x] == '.')
+			p->map[y - i][x] = c;
+		i++;
+	}
+}
+
+void			analyse_map(int fd, t_player *p)
+{
 	int y;
+	int x;
+	char c;
 
 	y = 0;
-	while (y < p->m_y)
+	x = 0;
+	c = '!';
+	while (y < p->m_y - 1)
 	{
 		x = 0;
 		while (p->map[y][x])
 		{
-			if (p->nb == 1 && (p->map[y][x] == 'X'))
-			{
-				p->op_y = y;
-				p->op_x = x;
-				break ;
-			}
-			else if (p->nb == 2 && (p->map[y][x] == 'O'))
-			{
-				p->op_y = y;
-				p->op_x = x;
-				break ;
-			}
+			if (p->map[y][x] == 'X')
+				fill_map(p, y, x, c);
 			x++;
 		}
 		y++;
+	}
+	while (c < '~')
+	{
+		c++;
+		if (c == '.' || c == 'O')
+			c += 2;
+		y = 0;
+		while (y < p->m_y - 1)
+		{
+			x = 0;
+			while (p->map[y][x])
+			{
+				if (c == '0' && p->map[y][x] == c - 3)
+					fill_map(p, y, x, c);
+				else if (c == 'Q' && p->map[y][x] == c - 3)
+					fill_map(p, y, x, c);
+				else if (p->map[y][x] == c - 1)
+					fill_map(p, y, x, c);
+				x++;
+			}
+			y++;
+		}
 	}
 }
