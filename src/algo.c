887099de75a5ad20090a4f8df14e9decd91f3a40
Sabri MAKNI <sabrimakni@gmail.com>
2019-01-23 15:33:25 +0100

    time ok

--- src/algo.c
+++ src/algo.c
@@ -6,7 +6,7 @@
 /*   By: smakni <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/14 12:20:12 by smakni            #+#    #+#             */
-/*   Updated: 2019/01/18 20:53:32 by smakni           ###   ########.fr       */
+/*   Updated: 2019/01/22 14:01:12 by smakni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,31 +26,28 @@ static int		test_piece(int fd, int m_y, int m_x, t_player *p)
 	check = 0;
 	while (y < p->p_y)
 	{
-			x = 0;
-			m_x = tmp_m_x;
-			if (ft_strchr(p->piece[y], '*') != 0)
+		x = 0;
+		m_x = tmp_m_x;
+		while (x < p->p_x)
+		{
+			if (p->piece[y][x] == '*')
 			{
-				while (x < p->p_x)
+				if (((m_y < 0 || m_y >= p->m_y
+					|| m_x < 0 || m_x >= p->m_x)
+					|| (check == 1 && p->map[m_y][m_x] == p->my_c)
+					|| p->map[m_y][m_x] == p->op_c))
+					return (-1);
+				else if (check == 0 && p->map[m_y][m_x] == p->my_c)
 				{
-					if (p->piece[y][x] == '*')
-					{
-						if (((m_y < 0 || m_y >= p->m_y
-							|| m_x < 0 || m_x >= p->m_x)
-							|| (check == 1 && p->map[m_y][m_x] == p->my_c)
-							|| p->map[m_y][m_x] == p->op_c))
-						return (-1);
-						else if (check == 0 && p->map[m_y][m_x] == p->my_c)
-						{
-							p->save += p->map[y][x];
-							check++;
-						}
-						else if (p->map[m_y][m_x] != p->my_c)
-								p->save += p->map[m_y][m_x];
-					}
+					p->save += p->map[y][x];
+					check++;
+				}
+				else if (p->map[m_y][m_x] != p->my_c)
+					p->save += p->map[m_y][m_x];
+				}
 					x++;
 					m_x++;
-				}
-			}
+		}
 		y++;
 		m_y++;
 	}
@@ -83,31 +80,34 @@ static	int		algo_0(int fd, t_player *p)
 				p->offset_x = 0;
 				tmp_y = 0;
 				tmp_y = 0;
-				while (p->offset_y <= p->p_y)
+				while (p->offset_y < p->p_y)
 				{
-					p->offset_x = 0;
-					while (p->offset_x <= p->p_x)
+					if (ft_strchr(p->piece[p->offset_y], '*') != 0)
 					{
-						tmp_y = y - p->offset_y;
-						tmp_x = x - p->offset_x;
-						if (test_piece(fd, tmp_y, tmp_x, p) == 0)
+						p->offset_x = 0;
+						while (p->offset_x < p->p_x)
 						{
-							if (check == 0)
+							tmp_y = y - p->offset_y;
+							tmp_x = x - p->offset_x;
+							if (test_piece(fd, tmp_y, tmp_x, p) == 0)
 							{
-								save_path = p->save;
-								p->r_y = tmp_y;
-								p->r_x = tmp_x;
-								check++;
+								if (check == 0)
+								{
+									save_path = p->save;
+									p->r_y = tmp_y;
+									p->r_x = tmp_x;
+									check++;
+								}
+								else if (check > 0 && p->save < save_path)
+								{
+									save_path = p->save;
+									p->r_y = tmp_y;
+									p->r_x = tmp_x;
+								}
 							}
-						else if (check > 0 && p->save < save_path)
-							{
-								save_path = p->save;
-								p->r_y = tmp_y;
-								p->r_x = tmp_x;
-							}
+							p->save = 0;
+							p->offset_x++;
 						}
-						p->save = 0;
-						p->offset_x++;
 					}
 					p->offset_y++;
 				}
